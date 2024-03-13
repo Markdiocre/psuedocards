ac0ddc3a9371374e29f2a4b4363613eff08b0503
Markdiocre <202010597@gordoncollege.edu.ph>
2024-03-14 01:02:27 +0800

    Final commit?

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,9 +40,14 @@ function animateFrom(elem, direction) {
   });
 }
 
+function goToItch() {
+  window.location.assign('https://markdiocre.itch.io/psuedocards')
+}
+
 function hide(elem) {
   gsap.set(elem, { autoAlpha: 0 });
 }
+
 
 onMounted(() => {
 
@@ -81,17 +86,17 @@ onMounted(() => {
     <div class="title">
       <h1>PSUEDOCARDS</h1>
     </div>
-    <div class="itch">
+    <div class="itch" @click="goToItch()">
       <img src="/itch.svg" alt="">
     </div>
+
   </nav>
-  <main class="crt">
+  <main>
     <div id="hero">
-      <div class="">
+      <div class="crt">
         <h1 data-speed="0.75" class="title-main">PSUEDOCARDS</h1>
         <p data-speed="1.2">A card based game for algorithm simulation using Godot Engine</p>
       </div>
-
     </div>
     <div id="promotion" data-scroll-section>
 
@@ -102,28 +107,170 @@ onMounted(() => {
         <div class="text-center desc" data-speed="1">
           <p>
             Test your algorithm-building skills in PsuedoCards!
-        </p>
-          
+          </p>
+
+        </div>
+      </div>
+    </div>
+
+    <div id="hows">
+      <div class="container">
+        <div class="col-lg-12">
+          <div class="row">
+            <h2 class="text-center h2">CHECK THE PSUEDO-CARDS</h2>
+          </div>
+          <div class="row">
+            <div class="col-md-4 mt-3">
+              <div class="hows-panel">
+                <h4>INPUT</h4>
+                <p>Accept an input and make it the current value</p>
+              </div>
+            </div>
+            <div class="col-md-4 mt-3">
+              <div class="hows-panel">
+                <h4>OUTPUT</h4>
+                <p>Send the current value to output</p>
+              </div>
+            </div>
+            <div class="col-md-4 mt-3">
+              <div class="hows-panel">
+                <h4>ARITHMETIC</h4>
+                <p>Perform Addition, Subtraction, Multiplication, Divison, or Modulo to a certain index value</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 mt-3">
+            <div class="hows-panel">
+              <h4>JUMP
+              </h4>
+              <p>Used to go to a card with previous connections</p>
+            </div>
+          </div>
+          <div class="col-md-3 mt-3">
+            <div class="hows-panel">
+              <h4>JUMP IF</h4>
+              <p>If conditions are met, the flow will go through THEN, if not then it will go through ELSE</p>
+            </div>
+          </div>
+          <div class="col-md-3 mt-3">
+            <div class="hows-panel">
+              <h4>STORE</h4>
+              <p>Store current value to an index value</p>
+            </div>
+          </div>
+          <div class="col-md-3 mt-3">
+            <div class="hows-panel">
+              <h4>COPY</h4>
+              <p>Copy from an index value and make it the current value</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="screenshot">
+      <div class="container">
+        <div class="col-lg-12">
+          <div class="row p-2">
+            <h2 class="text-center">HERE ARE SOME INGAME SCREENSHOTS!</h2>
+          </div>
+          <div class="row">
+            <div id="carouselExampleIndicators" class="carousel slide">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                  aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                  aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                  aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                  aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+                  aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
+                  aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"
+                  aria-label="Slide 7"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="/1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="/2.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="/3.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="/4.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="/5.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="/6.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="/7.png" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
-
-
-
-
     </div>
 
-    <div id="about">
+    <div id="itch" @click="goToItch()">
+      <h1 class="text-center">AVAILABLE ON ITCH.IO</h1>
+    </div>
+
+    <div id="footer">
       <div class="container">
         <div class="row">
-          <div class="col-lg-12">
-            YOOO
+          <div class="col-lg-4">
+            <div class="d-flex justify-content-center align-items-center">
+              <div>
+                <h4>DEVELOPED BY:</h4>
+                <p>MARK THADDEUS R. MANUEL</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="d-flex justify-content-center align-items-center">
+              <div>
+                <div><img src="/vvl.png" height="70px"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="d-flex">
+              <a class="icon" href="https://www.facebook.com/PsuedoCards"><svg xmlns="http://www.w3.org/2000/svg" width="32"
+                  height="32" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                  <path
+                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                </svg></a>
+
+                <div @click="goToItch()" class="ps-2" style="cursor: pointer;"> <img src="/app-icon.svg" alt="" width="32px"></div >
+            </div>
           </div>
         </div>
       </div>
     </div>
   </main>
-
 
 </template>
 
@@ -160,8 +307,6 @@ onMounted(() => {
 #nav .title h1 {
   font-family: TF;
   text-align: center;
-
-
 }
 
 #nav .itch img {
@@ -214,9 +359,8 @@ onMounted(() => {
 
 #promotion {
   background-color: black;
-  height: 100%;
+  height: 500px;
   width: 100%;
-
 }
 
 .desc {
@@ -243,10 +387,11 @@ onMounted(() => {
 
 .vid {
   width: 100%;
+  height: 100%;
 }
 
 .vid video {
-  max-height: 500px;
+  max-height: 100%;
   width: 100%;
   display: inline-block;
   vertical-align: baseline;
@@ -256,7 +401,67 @@ onMounted(() => {
 
 }
 
-#about{
+#about {
   height: 100vh;
+}
+
+.panel {
+  height: 100vh;
+}
+
+#hows {
+  height: 100%;
+  color: white;
+  background-color: #1e1e1e;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.hows-panel {
+  border: #96ce00 1px solid;
+  height: 100%;
+  padding: 10px;
+}
+
+.hows-panel:hover {
+  background-color: #96ce00;
+  color: #1e1e1e;
+  transform: scale(1.1);
+  transition: 0.2s ease;
+  cursor: pointer;
+
+}
+
+p {
+  margin: 0px;
+}
+
+#screenshot {
+  color: white;
+  background-color: #1e1e1e;
+  padding: 20px;
+}
+
+#itch {
+  background-color: #FF244A;
+}
+
+#itch h1 {
+  margin: 0px;
+}
+
+#itch:hover {
+  color: white;
+  background-color: #96ce00;
+  cursor: pointer;
+}
+
+#footer {
+  padding: 20px;
+  text-decoration: none;
+}
+
+#footer .icon svg{
+  height: 32px;
 }
 </style>
