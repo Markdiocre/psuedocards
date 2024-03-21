602bf10dc9b94604f5b46c9d6c23e7203aab82f1
Markdiocre <202010597@gordoncollege.edu.ph>
2024-03-21 22:53:52 +0800

    Added page views counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -246,6 +246,11 @@ onMounted(() => {
               <div>
                 <h4>DEVELOPED BY:</h4>
                 <p>MARK THADDEUS R. MANUEL</p>
+                <p>Page Views: 
+
+                  <a href="https://www.hitwebcounter.com" target="_blank">
+<img src="https://hitwebcounter.com/counter/counter.php?page=12131667&style=0008&nbdigits=5&type=page&initCount=0" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></a>          
+                </p>
               </div>
             </div>
           </div>
